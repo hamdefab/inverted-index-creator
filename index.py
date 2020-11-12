3affea54f1346156272399d161aaafca3c8612fa
Hamza Khalique <hkhaliqu@uci.edu>
2020-11-12 00:04:38 -0800

    Add files via upload

--- index.py
+++ index.py
@@ -1,18 +1,30 @@
+import json
 import re
+import string
 import sys
 from mmap import ACCESS_READ, mmap    
 import ijson
 import glob
+import nltk
+from collections import defaultdict
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import snowball
+nltk.download('punkt')
 
 def generate_tokens():
-    # with open(filename) as f, mmap(f.fileno(), 0, access=ACCESS_READ) as mm:
-    #     yield from re.finditer(pattern, mm)cd hw2
-    for file_name in [file for file in glob.glob("~/cs121/hw3/DEV/*/.JSON")]:
-        for prefix, the_type, value in ijson.parse(file_name)):
-            print (prefix, the_type, value)
+    punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
+
+    for file_name in [file for file in glob.glob(r"C:\Users\hamza\OneDrive\Desktop\DEV\**\*json")]:
+        for url, encoding, content in ijson.parse(open(file_name)):
+            text = json.dumps(content)
+            text_tokens = word_tokenize(text)
+            # for each in text_tokens:
+            #     if each in punc:
+            #         text_tokens = text_tokens.filter(each)
+            print(text_tokens)
 
 def main():
-    
+   generate_tokens()
 
 if __name__ == "__main__":
     main()
