ac5d8b80340d87a0c1030283a811b921c25066f9
Hamza Midhat Abdul Khalique <hkhaliqu@circinus-44.ics.uci.edu>
2020-11-11 19:10:20 -0800

    commit

--- index.py
+++ index.py
@@ -1,7 +1,18 @@
 import re
 import sys
 from mmap import ACCESS_READ, mmap    
+import ijson
+import glob
 
-def generate_tokens(filename, pattern):
-    with open(filename) as f, mmap(f.fileno(), 0, access=ACCESS_READ) as mm:
-         yield from re.finditer(pattern, mm)
+def generate_tokens():
+    # with open(filename) as f, mmap(f.fileno(), 0, access=ACCESS_READ) as mm:
+    #     yield from re.finditer(pattern, mm)cd hw2
+    for file_name in [file for file in glob.glob("~/cs121/hw3/DEV/*/.JSON")]:
+        for prefix, the_type, value in ijson.parse(file_name)):
+            print (prefix, the_type, value)
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
