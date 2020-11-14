2805b808d6fa346ca8cd5c36c6c123a5c8a5fa06
Hamza Khalique <hkhaliqu@uci.edu>
2020-11-13 20:46:38 -0800

    Add files via upload

--- index.py
+++ index.py
@@ -3,11 +3,13 @@ import re
 import sys
 from mmap import ACCESS_READ, mmap    
 import ijson
+import os
 import json
 import glob
 import nltk
 from collections import defaultdict
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import snowball
 from bs4 import BeautifulSoup
 import string
 
@@ -37,23 +39,48 @@ def generate_tokens(file):
             for h in soup.find_all('h1', 'h2', 'h3'):
                 theText += h.string
             text = soup.get_text()
-
     corpus = []
     punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~©'''
     for each in text:
         if each in punc:
             text = text.replace(each," ")
 
+
+    #stemming = snowball.SnowballStemmer('english')
+    text = text.lower()
+    # for docid, c in enumerate(text):
+    #     for s in sent_tokenize(text):
+    #         token_words = word_tokenize(s)
+    #         stem = stemming.stem(str(token_words))
+    #         inverted_index[token_words].add(docid)
+    #
+    #         #corpus += token_words
     for s in sent_tokenize(text):
         token_words = word_tokenize(s)
-        # for t in token_words:
-        #     if t.isalnum()==True:
         corpus += token_words
+
+    inverted_index = {}
+    index = open("inverted_index.txt", "a+", encoding="utf-8")
+    # for docid, c in text:
+    #     for sent in sent_tokenize(c):
+    #         for word in word_tokenize(sent):
+    #             word_lower = word.lower()
+    #             word_stem = stemming.stem(word_lower)
+    #             inverted_index[word_stem].add(docid)
+
+    for item in corpus:
+        if item not in inverted_index:
+            inverted_index[item] = 1
+        if item in inverted_index:
+            inverted_index[item] += 1
+
+    index.write(str(inverted_index))
     return corpus
 
 def reportFunc(tokens):
     file1 = open("file1.txt", "a+" , encoding ="utf-8")
     file2 = open("file2.txt", "a+" , encoding ="utf-8")
+
     for token in range(len(tokens)):
         if token + 6 < len(tokens):
             file1.write(tokens[token] + " ")
@@ -62,20 +89,31 @@ def reportFunc(tokens):
             file1.write(tokens[token + 3] + " ")
             file1.write(tokens[token + 4] + " " + "\n")
             in_file = False
-            for line in file2.readlines():
-                if tokens[token] == line:
+            file2.seek(0)
+            lines = file2.readlines()
+            for line in lines:
+                if tokens[token] == line.strip("\n"):
                     in_file = True
             if in_file == False:
                 file2.write(tokens[token] + "\n")
-    #os.path.getsize()
+
+    file2.seek(0)
+    length = len(file2.readlines())
+
+    file1.close()
+    file2.close()
+    return length
 
 def main():
     count = 0
     for folder in paths:
         #print(folder)
         corpus = generate_tokens(folder)
-        reportFunc(corpus)
+        length_of_unique = reportFunc(corpus)
         count+= 1
+    report = open("report.txt", "a+", encoding="utf-8")
+    report.write("Number of Indexed Documents: " + count + "\n")
+    report.write("The number of unique word: " + length_of_unique + "\n")
 
 if __name__ == "__main__":
     main()
